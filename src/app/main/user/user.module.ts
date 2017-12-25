7097eb4f00e8ed5deb73388302072c2aaeb98312
englsa <arsenal4990@gmail.com>
2017-12-25 23:10:30 +0700

    lesson28: Upload Images

--- src/app/main/user/user.module.ts
+++ src/app/main/user/user.module.ts
@@ -4,6 +4,7 @@ import { UserComponent } from './user.component';
 import { Routes, RouterModule } from '@angular/router';
 import { DataService } from '../../core/services/data.service';
 import { NotificationService } from '../../core/services/notification.service';
+import { UploadService } from '../../core/services/upload.service';
 import { PaginationModule,ModalModule } from 'ngx-bootstrap';
 import {FormsModule} from '@angular/forms'
 import { Daterangepicker } from 'ng2-daterangepicker';
@@ -23,6 +24,6 @@ const userRoutes :Routes =[
     RouterModule.forChild(userRoutes)
   ],
   declarations: [UserComponent],
-  providers:[DataService,NotificationService]
+  providers:[DataService,NotificationService,UploadService]
 })
 export class UserModule { }
