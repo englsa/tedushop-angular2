7d4bddd4cb8103fb1f77901044ec24ed61357169
englsa <arsenal4990@gmail.com>
2018-01-04 17:30:54 +0700

    lesson:37

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,93 +1,84 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Http, Response, Headers } from '@angular/http';
 import { Router } from '@angular/router';
 import { SystemConstants } from './../common/system.constants';
 import { AuthenService } from './authen.service';
 import { NotificationService } from './notification.service';
 import { UltilityService } from './ultility.service';
+
 import { Observable } from 'rxjs/Observable';
 import { MessageContstants } from './../common/message.constants';
+
 @Injectable()
 export class DataService {
-  private headers: Headers
+  private headers: Headers;
   constructor(private _http: Http, private _router: Router, private _authenService: AuthenService,
-    private _notificationService: NotificationService, private _ultilityService: UltilityService
-  ) {
+    private _notificationService: NotificationService, private _utilityService: UltilityService) {
     this.headers = new Headers();
     this.headers.append('Content-Type', 'application/json');
   }
-  // get(uri: string) {
-  //   this.headers.delete("Authorization");
-  //   this.headers.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().access_token)
-  //   return this._http.get(SystemConstants.BASE_API + uri, { headers: this.headers }).map(this.extractData);
 
-  // }
   get(uri: string) {
     this.headers.delete("Authorization");
     this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
     return this._http.get(SystemConstants.BASE_API + uri, { headers: this.headers }).map(this.extractData);
   }
-  // post(uri: string, data?: any) {
-  //   this.headers.delete("Authorization");
-  //   this.headers.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().access_token)
-  //   return this._http.post(SystemConstants.BASE_API + uri, data, { headers: this.headers }).map(this.extractData);
-  // }
   post(uri: string, data?: any) {
     this.headers.delete("Authorization");
     this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
     return this._http.post(SystemConstants.BASE_API + uri, data, { headers: this.headers }).map(this.extractData);
   }
-  // put(uri: string, data?: any) {
-  //   this.headers.delete("Authorization");
-  //   this.headers.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().access_token)
-  //   return this._http.put(SystemConstants.BASE_API + uri, data, { headers: this.headers }).map(this.extractData);
-  // }
   put(uri: string, data?: any) {
     this.headers.delete("Authorization");
     this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
     return this._http.put(SystemConstants.BASE_API + uri, data, { headers: this.headers }).map(this.extractData);
   }
-  // delete(uri: string, key: string, id: string) {
-  //   this.headers.delete("Authorization");
-  //   this.headers.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().access_token)
-  //   return this._http.delete(SystemConstants.BASE_API + uri + "/?" + key + "=" + id, { headers: this.headers }).map(this.extractData);
-  // }
   delete(uri: string, key: string, id: string) {
     this.headers.delete("Authorization");
     this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
     return this._http.delete(SystemConstants.BASE_API + uri + "/?" + key + "=" + id, { headers: this.headers })
       .map(this.extractData);
   }
-  // postFile(uri: string, data?: any) {
-  //   let newHeader = new Headers();
-  //   newHeader.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().access_token)
-  //   return this._http.get(SystemConstants.BASE_API + uri, { headers: newHeader }).map(this.extractData);
+  deleteWithMultiParams(uri: string, params) {
+    this.headers.delete('Authorization');
 
-  // }
+    this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
+    var paramStr: string = '';
+    for (let param in params) {
+      paramStr += param + "=" + params[param] + '&';
+    }
+    return this._http.delete(SystemConstants.BASE_API + uri + "/?" + paramStr, { headers: this.headers })
+      .map(this.extractData);
+
+  }
   postFile(uri: string, data?: any) {
     let newHeader = new Headers();
     newHeader.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
     return this._http.post(SystemConstants.BASE_API + uri, data, { headers: newHeader })
       .map(this.extractData);
   }
-  
   private extractData(res: Response) {
     let body = res.json();
     return body || {};
   }
-   public handleError(error: any) {
-        if (error.status == 401) {
-            localStorage.removeItem(SystemConstants.CURRENT_USER);
-            this._notificationService.printErrorMessage(MessageContstants.LOGIN_AGAIN_MSG);
-            this._ultilityService.navigateToLogin();
-        }
-        else {
-            let errMsg = (error.message) ? error.message :
-                error.status ? `${error.status} - ${error.statusText}` : 'Lỗi hệ thống';
-            this._notificationService.printErrorMessage(errMsg);
-
-            return Observable.throw(errMsg);
-        }
-
+  public handleError(error: any) {
+    if (error.status == 401) {
+      localStorage.removeItem(SystemConstants.CURRENT_USER);
+      this._notificationService.printErrorMessage(MessageContstants.LOGIN_AGAIN_MSG);
+      this._utilityService.navigateToLogin();
     }
+    else if (error.status == 403) {
+      localStorage.removeItem(SystemConstants.CURRENT_USER);
+      this._notificationService.printErrorMessage(MessageContstants.FORBIDDEN);
+      this._utilityService.navigateToLogin();
+    }
+    else {
+      let errMsg = JSON.parse(error._body).Message;
+      this._notificationService.printErrorMessage(errMsg);
+
+      return Observable.throw(errMsg);
+    }
+
+
+  }
 }
--- src/app/main/product/product.component.ts
+++ src/app/main/product/product.component.ts
@@ -30,11 +30,20 @@ export class ProductComponent implements OnInit {
   public products: any[];
   public productCategories: any[];
   public checkedItems: any[];
- /*Product manage */
- public imageEntity: any = {};
- public productImages: any = [];
- @ViewChild('imageManageModal') public imageManageModal: ModalDirective;
- @ViewChild("imagePath") imagePath;
+  /*Product manage */
+  public imageEntity: any = {};
+  public productImages: any = [];
+  @ViewChild('imageManageModal') public imageManageModal: ModalDirective;
+  @ViewChild("imagePath") imagePath;
+  public sizeId: number = null;
+  public colorId: number = null;
+  public colors: any[];
+  public sizes: any[];
+
+  /*Quantity manage */
+  @ViewChild('quantityManageModal') public quantityManageModal: ModalDirective;
+  public quantityEntity: any = {};
+  public productQuantities: any = [];
 
   constructor(public _authenService: AuthenService,
     private _dataService: DataService,
@@ -178,5 +187,54 @@ export class ProductComponent implements OnInit {
         });
       }
     }
+  }
+  /*Quản lý số lượng */
+  public showQuantityManage(id: number) {
+    this.quantityEntity = {
+      ProductId: id
+    };
+    this.loadColors();
+    this.loadSizes();
+    this.loadProductQuantities(id);
+    this.quantityManageModal.show();
+
+  }
+  public loadColors() {
+    this._dataService.get('/api/productQuantity/getcolors').subscribe((response: any[]) => {
+      this.colors = response;
+    }, error => this._dataService.handleError(error));
+  }
+  public loadSizes() {
+    this._dataService.get('/api/productQuantity/getsizes').subscribe((response: any[]) => {
+      this.sizes = response;
+    }, error => this._dataService.handleError(error));
+  }
+
+  public loadProductQuantities(id: number) {
+    this._dataService.get('/api/productQuantity/getall?productId=' + id + '&sizeId=' + this.sizeId + '&colorId=' + this.colorId).subscribe((response: any[]) => {
+      this.productQuantities = response;
+    }, error => this._dataService.handleError(error));
+  }
+
+  public saveProductQuantity(isValid: boolean) {
+    if (isValid) {
+      this._dataService.post('/api/productQuantity/add', JSON.stringify(this.quantityEntity)).subscribe((response: any) => {
+        this.loadProductQuantities(this.quantityEntity.ProductId);
+        this.quantityEntity = {
+          ProductId: this.quantityEntity.ProductId
+        };
+        this.notificationService.printSuccessMessage(MessageContstants.CREATED_OK_MSG);
+      }, error => this._dataService.handleError(error));
+    }
+  }
+
+   public deleteQuantity(productId: number, colorId: string, sizeId: string) {
+    var parameters = { "productId": productId, "sizeId": sizeId, "colorId": colorId };
+    this.notificationService.printConfirmationDialog(MessageContstants.CONFIRM_DELETE_MSG, () => {
+      this._dataService.deleteWithMultiParams('/api/productQuantity/delete', parameters).subscribe((response: any) => {
+        this.notificationService.printSuccessMessage(MessageContstants.DELETED_OK_MSG);
+        this.loadProductQuantities(productId);
+      }, error => this._dataService.handleError(error));
+    });
   }
 }
