02a49a1ebb987d9137b2086aa94bfcb3ca3444fb
englsa <arsenal4990@gmail.com>
2018-01-04 14:46:32 +0700

    lesson33: product-category

--- src/app/main/product-category/product-category.module.ts
+++ src/app/main/product-category/product-category.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductCategoryComponent } from './product-category.component';
+import { ProductCategoryRouter } from './product-category.routes';
+
+import { TreeModule } from 'angular-tree-component';
+import { FormsModule } from '@angular/forms';
+import { ModalModule } from 'ngx-bootstrap';
+import { DataService } from './../../core/services/data.service';
+import { UltilityService } from './../../core/services/ultility.service';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ProductCategoryRouter,
+    TreeModule,
+    ModalModule,
+    FormsModule
   ],
-  declarations: [ProductCategoryComponent]
+  declarations: [ProductCategoryComponent],
+  providers: [DataService, UltilityService]
 })
 export class ProductCategoryModule { }
